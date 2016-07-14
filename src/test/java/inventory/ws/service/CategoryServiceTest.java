e42449033f26c67f6fc3253bc3162210a2bb20cf
Luis Carlin <luiscarlin@gmail.com>
2016-07-13 23:05:01 -0400

    Add basic test and run on Travis CI

---
+++ src/test/java/inventory/ws/service/CategoryServiceTest.java
@@ -0,0 +1,12 @@
+package inventory.ws.service;
+
+import inventory.ws.AbstractTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CategoryServiceTest extends AbstractTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
